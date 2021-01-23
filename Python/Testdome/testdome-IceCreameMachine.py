c79e8600bfd6b2d999e9f8527fdddd45abfea548
Muhammad Ahsan <ahsan@vivaldi.net>
2021-01-24 01:18:47 +0200

    Add description in IceCreamMachine.py

--- Python/Testdome/testdome-IceCreameMachine.py
+++ Python/Testdome/testdome-IceCreameMachine.py
@@ -1,3 +1,10 @@
+'''
+Implement the IceCreamMachine's scroops method so that it returns all combinations of one ingredient and one topping. If
+there are no ingredients or toppings, the method should return an empty list.
+
+For example, IceCreamMachine(["vanilla", "chocolate"],["chocolate sauce"]).scoops() should return [['vanilla',
+'chocolate sauce'],['chocolate', 'chocolate sauce']]
+'''
 class IceCreamMachine:
     
     def __init__(self, ingredients, toppings):
